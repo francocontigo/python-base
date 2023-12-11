c383b9f7d66df99ed716ab1ead4d32ea40924393
francocontigo <ricardoaugustofranco@hotmail.com>
2023-12-11 09:26:59 -0300

    RotatingFileHander

--- day3/logs.py
+++ day3/logs.py
@@ -1,5 +1,6 @@
 import os
 import logging
+from logging import handlers
 
 log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
 
@@ -8,15 +9,26 @@ log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
 # normalmente o nome do script é usado, para setar o nível do logging é boa pratica usar a constante
 log = logging.Logger(__name__, log_level) 
 #level
-ch = logging.StreamHandler() # Streamhandler  = Console/terminal/stderr
-ch.setLevel(log_level)
+# ch = logging.StreamHandler() # Streamhandler  = Console/terminal/stderr
+
+# 10**6 = 1mb, backupCount=10 mantém os últimos arquivos(caso seja 1 vai apagaros mais antigos)
+fh = handlers.RotatingFileHandler(
+    "meulog.log", 
+    maxBytes=10**6, 
+    backupCount=10
+    ) 
+
+# ch.setLevel(log_level)
+fh.setLevel(log_level)
 #formatacao, ver documentação para ver todas as mascaras
 fmt = logging.Formatter(
     '%(asctime)s  %(name)s  %(levelname)s '
     'l:%(lineno)d f:%(filename)s: %(message)s'
 )
-ch.setFormatter(fmt)
-log.addHandler(ch)
+# ch.setFormatter(fmt)
+# log.addHandler(ch)
+fh.setFormatter(fmt)
+log.addHandler(fh)
 
 log.debug("Mensagem pro dev, qe, sysadmin")
 log.info("Mensagem geral para usuarios, informação atrelada ao sistema")
