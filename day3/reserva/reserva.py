29b5a14a4644461cfa971dbe923353bf80013d8b
francocontigo <ricardoaugustofranco@hotmail.com>
2024-02-06 17:29:56 -0300

    finalização exercicio de reservas

--- day3/reserva/reserva.py
+++ day3/reserva/reserva.py
@@ -1,6 +1,18 @@
 import sys
 import logging
 
+ocupados = {}
+try:
+    for line in open("reservas.txt"):
+        nome, num_quarto, dias = line.strip().split(",")
+        ocupados[int(num_quarto)] = {
+            "nome": nome,
+            "dias": dias
+        }
+except FileNotFoundError:
+    logging.error("Arquivo reservas.txt não existe.")
+    sys.exit(1)
+
 quartos = {}
 try:
     for line in open("quartos.txt"):
@@ -8,19 +20,52 @@ try:
         quartos[int(codigo)] = {
             "nome": nome,
             "preco": float(preco), # TODO: Decimal
-            "disponivel" : True
+            "disponivel" : False if int(codigo) in ocupados else True
         }
 except FileNotFoundError:
-    logging.error("Arquivo não encontrado")
+    logging.error("Arquivo quartos.txt não existe.")
     sys.exit(1)
-    
-for codigo, dados in quartos.items:
-    nome = dados["nome"]
-    preco = dados["preco"]
-    #disponivel = "🚫" if not dados["disponivel"] else "👍"
-    disponivel = dados["disponivel"] and "👍" or "🚫" # retorno com and é sempre o último avaliado
-    # TODO: Substituir casa decimal por virgula
-    print(f"{codigo} - {nome} - R$ {preco:.2f} - {disponivel}")
 
+if len(ocupados) == len(quartos):
+    print("Hotel lotado.")
+    sys.exit(1)
+
+print("Reserva Hotel Pythonico")
+print("-" * 40)
+print("Lista de quartos disponíveis:")
+for codigo, dados in quartos.items:
+    nome_quarto = dados["nome"]
+    preco = dados["preco"]
+    disponivel = "🚫" if not dados["disponivel"] else "👍"
+    #disponivel = dados["disponivel"] and "👍" or "🚫" # retorno com and é sempre o último avaliado
+    # TODO: Substituir casa decimal por virgula
+    print(f"{codigo} - {nome_quarto} - R$ {preco:.2f} - {disponivel}")
+
+print("-" * 40)
 nome = input("Nome do cliente:").strip()
-quarto = int(input("Número do quarto").strip())
+try:
+    num_quarto = int(input("Número do quarto: ").strip())
+    if not quartos[num_quarto]["disponível"]:
+        print(f"O quarto {num_quarto} está ocupado.")
+        sys.exit(1)
+except ValueError:
+    logging.error("Número inválido!")
+    sys.exit(1)
+except KeyError:
+    print(f"O quarto {num_quarto} não existe.")
+    sys.exit(1)
+
+try:
+    dias = int(input("Quantos dias? ").strip())
+except ValueError:
+    logging.error("Número inválido!")
+    sys.exit(1)
+
+nome_quarto = quartos[num_quarto]["nome"]
+preco_quarto = quartos[num_quarto]["preco"]
+total = preco_quarto * dias
+
+with open("reservas.txt", "a") as file_:
+    file_.write(f"{nome},{num_quarto},{dias}\n")
+
+print(f"{nome} você escolheu o quarto {nome_quarto} e vai custar: R${total:.2f}")
