a4aa789b7d77560c2b7c25c43f47b421a48dbfd9
francocontigo <ricardoaugustofranco@hotmail.com>
2024-06-17 10:20:09 -0300

    Enum, Super, Herança com dataclass

---
+++ modern_python_andtype_annotations/bond.py
@@ -0,0 +1,15 @@
+from instruments import Instrument, Guitar, Flute, ElectricGuitar
+
+# x = Instrument()
+
+gianini = Guitar("Gianini m2", colors=["green"])
+print(gianini.play())
+print(gianini.colors)
+
+
+yamaha = Flute("Yamaha Magic Flute", colors=["silver"])
+print(yamaha.play())
+print(yamaha.colors)
+
+lespaul = ElectricGuitar("lespaul m1")
+print(lespaul.play())
---
+++ modern_python_andtype_annotations/instruments.py
@@ -0,0 +1,62 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+from enum import Enum
+from typing import List
+
+# Enum
+class InstrumentKind(str, Enum):
+    string = "string"
+    wind = "wind"
+    keys = "keys"
+    drums = "drums"
+
+
+class ABCInstrument(ABC): # comportamento
+
+    @abstractmethod
+    def play(self): ...
+
+
+@dataclass
+class DataInstrumentMixin:  # deve ser usado junto com outra classe, dados
+    name: str
+    kind: str
+    sound: InstrumentKind
+    colors: List[str] = field(default_factory=list)
+
+
+class Instrument(DataInstrumentMixin, ABCInstrument):
+    ...
+
+
+@dataclass
+class Guitar(Instrument):
+    n_strings: int = 6
+    sound: str = "Ding ding ding"
+    kind: InstrumentKind = InstrumentKind.string
+    colors: List[str] = field(default_factory=lambda: ["red", "black"])
+
+    def play(self):
+        return self.sound
+
+
+@dataclass
+class ElectricGuitar(Guitar):
+    sound: str = "Wah wah wah"
+
+    def play(self, distortion="wave"):
+        return_from_base_class = super().play() # mro, olha da esquerda para direita as importações
+        if distortion == "wave":
+            return "~~~~".join(return_from_base_class.split())
+        return return_from_base_class
+
+
+
+@dataclass
+class Flute(Instrument):
+    sound: str = "Flu flu flu"
+    kind: InstrumentKind = InstrumentKind.wind
+    colors: List[str] = field(default_factory=lambda: ["beige", "white"])
+
+    def play(self):
+        return self.sound
